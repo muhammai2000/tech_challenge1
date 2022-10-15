0239f39189c8b97ff6108ee382b822cd5645db5d
Ihsan <muhammai2000@gmail.com>
2022-10-15 11:02:20 -0400

    DataPassport Tech challenge

--- components/MyCard/index.tsx
+++ components/MyCard/index.tsx
@@ -1,13 +1,16 @@
-import { Paper, Box } from "@mui/material";
+import { Paper, Box, SpeedDialAction } from "@mui/material";
+import { setDefaultResultOrder } from "dns";
 import React, { useEffect, useState } from "react";
+import seedData from "../../seedData.js"
 
 export default function MyCard() {
+  const {products} = seedData
+  const [ product, setProducts] = useState({})
+  console.log(product)
   useEffect(() => {
     fetch("https://dummyjson.com/products?limit=10")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      })
+      .then(response => response.json())
+      .then(data =>  setProducts(data.products))
       .catch((err) => {
         console.log(err.message);
       });
@@ -26,8 +29,24 @@ export default function MyCard() {
     >
       <img src="/DataPassportsLogo.svg" height="100" width="300" />
       <Box>
-        {/* Start here */}
+        <div className="datapass">
+          {
+          products.map((data) => {
+
+            return (
+              <div>
+                <h4>{data.title}</h4>
+                <p>{data.price}</p>
+                <img src={`${data.thumbnail}`} alt="" />
+                {data.thumbnail}
+              </div>
+            )
+          })
+        }
+        </div>
+         
       </Box>
     </Paper>
+
   );
 }
